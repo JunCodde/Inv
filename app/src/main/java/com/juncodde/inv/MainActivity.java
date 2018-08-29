2e5b4323267a11437e3f437e70e75548c2a3bcf9
Junior Ricci <junior2mil99@gmail.com>
2018-08-29 14:36:58 -0400

    add - Activity Inventario

--- app/src/main/java/com/juncodde/inv/MainActivity.java
+++ app/src/main/java/com/juncodde/inv/MainActivity.java
@@ -1,13 +1,34 @@
 package com.juncodde.inv;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    CardView cv_Inventario, cv_ventas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cv_Inventario = (CardView) findViewById(R.id.cv_Inventario);
+        cv_ventas = (CardView) findViewById(R.id.cv_ventas);
+
+
+        cv_Inventario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(MainActivity.this, InventarioActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(i);
+                finish();
+
+            }
+        });
+
     }
 }
